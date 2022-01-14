9d15920e2f14e0fab97b176ed2da3f0aee61f368
Bhavik Kumar <bhavikk85@gmail.com>
2022-01-14 16:11:42 +1300

    Add tests

--- jest.config.ts
+++ jest.config.ts
@@ -2,5 +2,6 @@ export default {
     clearMocks: true,
     collectCoverage: true,
     coverageDirectory: 'reports/coverage',
-    coverageProvider: 'v8'
+    coverageProvider: 'v8',
+    testResultsProcessor: 'jest-sonar-reporter'
 };
