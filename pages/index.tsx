e6e3a74288b26eb480a90f187d07235b696fa9dd
Bhavik Kumar <bhavikk85@gmail.com>
2022-04-21 11:47:19 +1200

    Add server side props for testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,26 @@
-const Home = function NextPage() {
+import Link from 'next/link';
+import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+    return {
+        props: {
+            uri: `This should work...`
+        }
+    };
+};
+
+const Home = function NextPage({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     return (
         <main>
             <h1>Welcome to Next.JS</h1>
+            <p>Print {data.uri}</p>
             <p>
                 Please look at the following link for NextJS{' '}
-                <a href="https://nextjs.org/docs/getting-started" target="_blank" rel="noreferrer">
-                    getting started
-                </a>
+                <Link href="https://nextjs.org/docs/getting-started">
+                    <a target="_blank" rel="noreferrer">
+                        getting started
+                    </a>
+                </Link>
             </p>
         </main>
     );
