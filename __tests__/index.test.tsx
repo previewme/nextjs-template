eabbc847f5b3242f1ec6bd596d2d502959d8e0fc
Bhavik Kumar <bhavikk85@gmail.com>
2022-04-21 12:00:38 +1200

    Update tests to pass uri

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -3,7 +3,7 @@ import Home from '@/pages/index';
 
 describe('Home', () => {
     it('renders a heading', () => {
-        render(<Home />);
+        render(<Home data={{ uri: '' }} />);
 
         const heading = screen.getByRole('heading', {
             name: 'Welcome to Next.JS'
