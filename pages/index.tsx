bb4b141fb83fd0bebb16e4b302b9e5e5f9900816
Bhavik Kumar <bhavikk85@gmail.com>
2022-04-26 12:47:56 +1200

    Add link with SSR in template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
-const Home = function NextPage() {
+import Link from 'next/link';
+import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+    if (!context.req) {
+        return { props: {} };
+    }
+    return {
+        props: {
+            test: 'test'
+        }
+    };
+};
+
+const Home = function NextPage({ test }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    console.info(JSON.stringify(test));
+
     return (
         <main>
             <h1>Welcome to Next.JS</h1>
             <p>
                 Please look at the following link for NextJS{' '}
-                <a href="https://nextjs.org/docs/getting-started" target="_blank" rel="noreferrer">
-                    getting started
-                </a>
+                <Link href="https://nextjs.org/docs/getting-started" passHref>
+                    <a target="_blank" rel="noreferrer">
+                        getting started
+                    </a>
+                </Link>
             </p>
         </main>
     );
