34e14800b39dd8f364470361668ba753b407da7e
Bhavik Kumar <bhavikk85@gmail.com>
2022-04-26 13:31:48 +1200

    Revert "Add link with SSR in template"
    This reverts commit bb4b141fb83fd0bebb16e4b302b9e5e5f9900816.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,12 @@
-import Link from 'next/link';
-import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
-
-export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
-    if (!context.req) {
-        return { props: {} };
-    }
-    return {
-        props: {
-            test: 'test'
-        }
-    };
-};
-
-const Home = function NextPage({ test }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-    console.info(JSON.stringify(test));
-
+const Home = function NextPage() {
     return (
         <main>
             <h1>Welcome to Next.JS</h1>
             <p>
                 Please look at the following link for NextJS{' '}
-                <Link href="https://nextjs.org/docs/getting-started" passHref>
-                    <a target="_blank" rel="noreferrer">
-                        getting started
-                    </a>
-                </Link>
+                <a href="https://nextjs.org/docs/getting-started" target="_blank" rel="noreferrer">
+                    getting started
+                </a>
             </p>
         </main>
     );
