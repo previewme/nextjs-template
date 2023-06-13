334e51b75ca3026924a18133c6a543191f8322c7
Jhunnel <jhunnel@previewme.com>
2023-06-13 15:55:26 +0800

    updated tg

--- deployment/service/networking.tf
+++ deployment/service/networking.tf
@@ -37,12 +37,7 @@ resource "aws_lb_listener_rule" "application_rule" {
     type = "forward"
     forward {
       target_group {
-        arn    = "arn:aws:elasticloadbalancing:us-east-1:371032233725:targetgroup/blue-tg-nextjs-template/5d2194a53daad8eb"
-        weight = 100
-      }
-      target_group {
-        arn    = "arn:aws:elasticloadbalancing:us-east-1:371032233725:targetgroup/green-tg-nextjs-template/c550aaf79ee60683"
-        weight = 0
+        arn = "arn:aws:elasticloadbalancing:us-east-1:371032233725:targetgroup/blue-tg-nextjs-template/5d2194a53daad8eb"
       }
     }
   }
