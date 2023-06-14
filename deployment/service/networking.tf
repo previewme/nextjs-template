ad91c5305e643b98613900515429b90eafa89475
Jhunnel <jhunnel@previewme.com>
2023-06-14 10:20:08 +0800

    blue tg all traffic

--- deployment/service/networking.tf
+++ deployment/service/networking.tf
@@ -34,17 +34,8 @@ resource "aws_lb_listener_rule" "application_rule" {
   listener_arn = data.terraform_remote_state.common.outputs.public_lb_https_listener
 
   action {
-    type = "forward"
-    forward {
-      target_group {
-        arn    = "arn:aws:elasticloadbalancing:us-east-1:371032233725:targetgroup/blue-tg-nextjs-template/5d2194a53daad8eb"
-        weight = 100
-      }
-      target_group {
-        arn    = "arn:aws:elasticloadbalancing:us-east-1:371032233725:targetgroup/green-tg-nextjs-template/c550aaf79ee60683"
-        weight = 0
-      }
-    }
+    type             = "forward"
+    target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:371032233725:targetgroup/blue-tg-nextjs-template/5d2194a53daad8eb"
   }
 
   condition {
@@ -66,8 +57,7 @@ resource "aws_lb_listener_rule" "application_rule" {
 
   lifecycle {
     ignore_changes = [
-      action[0].forward[0].target_group.0.weight,
-      action[0].forward[1].target_group.0.weight
+      action.0.target_group_arn
     ]
   }
 }
